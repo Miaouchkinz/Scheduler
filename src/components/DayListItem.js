7dcffaa29e430ec64125878f5d6af0c116a01cf9
Gab Richard <g.elodierichard@gmail.com>
2020-02-17 17:00:52 -0500

    remove comments

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,9 +2,6 @@ import React from "react";
 import classnames from "classnames/bind";
 import "components/DayListItem.scss";
 
-// Write a function called formatSpots in the DayListItem
-// component that will format the props.spots to make our tests pass.
-
 export default function DayListItem(props) {
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
@@ -28,7 +25,3 @@ export default function DayListItem(props) {
     </li>
   );
 }
-
-// Create an object called dayClass that applies the day-list__item--selected
-// class name if props.selected is true and the day-list__item--full
-// class name if props.spots is 0.
