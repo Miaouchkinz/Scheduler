627ae8ef48430087f4c239882e8d4635ea4d3f7d
Gab Richard <g.elodierichard@gmail.com>
2020-02-19 15:55:46 -0500

    implement useVisualMode for empty and show view

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,14 +2,27 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import { useVisualMode } from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> 
-      : <Empty/>}
+      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   );
 };
