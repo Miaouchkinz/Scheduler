0435956592539f355072427e5e20260007292e3c
Gab Richard <g.elodierichard@gmail.com>
2020-02-19 16:04:29 -0500

    implement transition CREATE and render Form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 import { useVisualMode } from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -16,11 +18,16 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === EMPTY && <Empty onAdd={transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
         />
       )}
     </article>
