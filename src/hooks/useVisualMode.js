79f68092c6dce3eb761ceee82f034e1bee2478e0
Gab Richard <g.elodierichard@gmail.com>
2020-02-19 15:38:40 -0500

    implement useVisualMode hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+export const useVisualMode = (initialMode) => {
+  const [currentMode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  const transition = (newMode, replace) => {
+    replace? setMode(newMode) :
+    setHistory([...history, currentMode]);
+    setMode(newMode);
+  }
+
+  const back = () => {
+    const newHistory = [...history]
+
+    setMode(newHistory.pop())
+    setHistory(newHistory)
+  }
+
+  return { mode: currentMode, transition, back };
+}
