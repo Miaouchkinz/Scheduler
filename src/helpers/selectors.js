dbd72bc40d9c6a84665366eb201db9d274dffb85
Gab Richard <g.elodierichard@gmail.com>
2020-02-18 19:22:15 -0500

    refactor getAppointmentsForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,5 @@
-export function getAppointmentsForDay(state, day) {
-  const selectedDay = day;
+export function getAppointmentsForDay(state, selectedDay) {
   const dayObject = state.days.find(day =>  day.name === selectedDay )
-  
   const result = dayObject ? dayObject.appointments.map(appointment => state.appointments[appointment]) : [];
   
   return result;
