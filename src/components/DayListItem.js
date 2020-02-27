444d1a47ec7ff52e46c24d16903200c2bbbb639d
Gab Richard <g.elodierichard@gmail.com>
2020-02-26 22:58:00 -0500

    deconstruct props object, clean up syntax

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,27 +2,27 @@ import React from "react";
 import classnames from "classnames/bind";
 import "components/DayListItem.scss";
 
-export default function DayListItem(props) {
+export default function DayListItem({ selected, spot, setDay, name, spots}) {
   const dayClass = classnames("day-list__item", {
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spot === 0
-  })
+    "day-list__item--selected": selected,
+    "day-list__item--full": spot === 0
+  });
 
   const formatSpots = (spots) => {
     if (spots === 0) return "no spots remaining"
     else if (spots === 1) return "1 spot remaining"
     else return `${spots} spots remaining`;
-  }
+  };
 
   return (
     <li 
       className={dayClass}
-      selected={props.selected}
-      onClick={props.setDay}
+      selected={selected}
+      onClick={setDay}
       data-testid="day"
     > 
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
-}
+};
