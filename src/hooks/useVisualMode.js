c4ce23303efc3314dd96a177d28467c40dddc72a
Gab Richard <g.elodierichard@gmail.com>
2020-02-26 23:07:07 -0500

    clean up syntax

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export const useVisualMode = (initialMode) => {
   const [currentMode, setMode] = useState(initialMode);
   const [history, setHistory] = useState([initialMode]);
 
   const transition = (newMode, replace) => {
-    replace? setMode(newMode) :
-    setHistory([...history, currentMode]);
+    replace? setMode(newMode) : setHistory([...history, currentMode]);
+
     setMode(newMode);
-  }
+  };
 
   const back = () => {
-    const newHistory = [...history]
+    const newHistory = [...history];
 
-    setMode(newHistory.pop())
-    setHistory(newHistory)
-  }
+    setMode(newHistory.pop());
+    setHistory(newHistory);
+  };
 
   return { mode: currentMode, transition, back };
-}
+};
