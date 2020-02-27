d8761d8d13807c8d80e337cd50a4e031aa86af16
Gab Richard <g.elodierichard@gmail.com>
2020-02-26 22:10:55 -0500

    add semicolons

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -64,7 +64,7 @@ export default {
         statusText: "OK",
         data: fixtures.daysList
       });
-    }
+    };
 
     if (url === "http://localhost:8001/api/appointments") {
       return Promise.resolve({
@@ -72,7 +72,7 @@ export default {
         statusText: "OK",
         data: fixtures.appointments
       });
-    }
+    };
 
     if (url === "http://localhost:8001/api/interviewers") {
       return Promise.resolve({
@@ -80,20 +80,20 @@ export default {
         statusText: "OK",
         data: fixtures.interviewers
       });
-    }
+    };
   }),
 
   put: jest.fn(url => {
       return Promise.resolve({
         status: 204,
         statusText: "No Content"
-    })
+    });
   }),
 
   delete: jest.fn(url => {
       return Promise.resolve({
         status: 204,
         statusText: "No Content"
-      })
+      });
   }),
-}
+};
